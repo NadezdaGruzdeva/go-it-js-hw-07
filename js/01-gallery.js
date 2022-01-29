bd634f15117aa6843d8ca14bac1abba217cbc70d
ng2015note4@gmail.com <ng2015note4@gmail.com>
2022-01-29 10:16:04 +0100

    add const options

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -24,35 +24,88 @@ function createGalleryItemCardMarkup(galleryItems) {
 }
 //проверка куда кликнули
 function onGalleryItemsContainerClick(evt) {
-    evt.preventDefault();
+  evt.preventDefault();
     
-    if (evt.target.nodeName !== "IMG") {
-        return;
-    }
-    const originalUrl = evt.target.dataset.source;
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  const originalUrl = evt.target.dataset.source;
+  
+
+
   //   const instance = basicLightbox.create(`<img src=${originalUrl}>`)
   // instance.show();
 
-   const instance = basicLightbox.create(`<img src=${originalUrl}>`, {
-      onShow: (instance) => { document.addEventListener('onShow', instance) },
-     onClose: (instance) => { document.removeEventListener('onClose', instance); console.log('blup') }
-    	})
-  instance.show();
-// console.log(evt.target)
+  //    const instance = basicLightbox.create(`<img src=${originalUrl}>`, {
+  //       onShow: (instance) => { document.addEventListener('onShow', instance) },
+  //      onClose: (instance) => { document.removeEventListener('onClose', instance); console.log('blup') }
+  //    })
+  
+  
+  //   instance.show();
+  // // console.log(evt.target)
 
-  //закрытие по ESC
-  document.addEventListener('keydown', function(event){
-    if (basicLightbox.visible() && event.key === "Escape") {
-      instance.close((instance) => { document.removeEventListener('keydown', instance); console.log('bla') });
-      // onClose: (instance) => { document.removeEventListener('keydown', instance) }
-      // console.log('bla' + evt.target);
-  //  document.removeEventListener('keydown', function(){console.log( 'hi')})
-    }
+  //   //закрытие по ESC
+  //   document.addEventListener('keydown', function(event){
+  //     if (basicLightbox.visible() && event.key === "Escape") {
+  //       instance.close((instance) => { document.removeEventListener('keydown', instance); console.log('bla') });
+  //       // onClose: (instance) => { document.removeEventListener('keydown', instance) }
+  //       // console.log('bla' + evt.target);
+  //   //  document.removeEventListener('keydown', function(){console.log( 'hi')})
+  //     }
     
-});
-    }
+  //   });
+  
 
-   
+  const options = {
+    onShow: () => {
+      window.addEventListener('keydown', onCloseEsc);
+    },
+    onClose: () => {
+      window.removeEventListener('keydown', onCloseEsc);
+    },
+
+
+  };
+
+  const instance = basicLightbox.create(`<img src=${originalUrl}>`, options);
+        instance.show();
+  function onCloseEsc(event) {
+          
+    if (basicLightbox.visible() && event.key === "Escape") {
+      instance.close();
+      console.log('kizf')
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // const onShow = window.addEventListener("keydown", onEscKeyPress);
   //const onClose = window.removeEventListener("keydown", onEscKeyPress);
